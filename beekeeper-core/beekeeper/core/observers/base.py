8b5a16e24362c4328ba630772eb90f50f6e5060e
Leonardo Furnielis <leonardofurnielis@Leonardos-MacBook-Pro16.local>
2025-10-29 00:25:20 -0300

    refactor: backward compability with observers

--- beekeeper-core/beekeeper/core/observers/base.py
+++ beekeeper-core/beekeeper/core/observers/base.py
@@ -1,46 +1,38 @@
 from abc import ABC, abstractmethod
 from typing import Optional
 
-from beekeeper.core.observers.types import PayloadRecord
+from beekeeper.core.monitors import BaseMonitor, PromptMonitor, TelemetryMonitor
+from beekeeper.core.monitors.types import PayloadRecord
 from beekeeper.core.prompts import PromptTemplate
 from deprecated import deprecated
 
 
-class BaseObserver(ABC):
-    """An interface for observability."""
-
-    @classmethod
-    def class_name(cls) -> str:
-        return "BaseObserver"
-
-
-class PromptObserver(BaseObserver):
-    """Abstract base class defining the interface for prompt observability."""
-
-    def __init__(self, prompt_template: Optional[PromptTemplate] = None) -> None:
-        self.prompt_template = prompt_template
-
-    @classmethod
-    def class_name(cls) -> str:
-        return "PromptObserver"
-
-    @abstractmethod
-    def __call__(self, payload: PayloadRecord) -> None:
-        """PromptObserver."""
+class BaseObserver(BaseMonitor):
+    """DEPRECATED: An interface for observability."""
 
 
 @deprecated(
-    reason="'ModelObserver()' is deprecated and will be removed in a future version. Use 'PromptObserver()' instead.",
+    reason="'PromptObserver()' is deprecated and will be removed in a future version. Use 'PromptMonitor()' instead.",
+    version="1.0.4",
+    action="always",
+)
+class PromptObserver(PromptMonitor):
+    """DEPRECATED: Abstract base class defining the interface for prompt observability."""
+
+
+@deprecated(
+    reason="'ModelObserver()' is deprecated and will be removed in a future version. Use 'PromptMonitor()' instead.",
     version="1.0.3",
     action="always",
 )
-class ModelObserver(PromptObserver):
+class ModelObserver(PromptMonitor):
     """DEPRECATED: This class is deprecated and kept only for backward compatibility."""
 
 
-class TelemetryObserver(BaseObserver):
-    """Abstract base class defining the interface for telemetry observability."""
-
-    @classmethod
-    def class_name(cls) -> str:
-        return "TelemetryObserver"
+@deprecated(
+    reason="'TelemetryObserver()' is deprecated and will be removed in a future version. Use 'TelemetryMonitor()' instead.",
+    version="1.0.4",
+    action="always",
+)
+class TelemetryObserver(BaseMonitor):
+    """DEPRECATED: Abstract base class defining the interface for telemetry observability."""
